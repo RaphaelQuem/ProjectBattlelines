794d81b070b34c86e7f0d3364891830bd2854471
Raphael <Raphzmaia@gmail.com>
2021-02-22 09:12:41 -0300

    GEt unit total

--- Assets/Model/BattlelineModel.cs
+++ Assets/Model/BattlelineModel.cs
@@ -1,8 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class BattlelineModel
 {
     public List<UnitModel> Deployments { get; set; }
+
+    public int GetTeamPower(TeamModel team)
+    {
+        var deployments = this.Deployments.Where(u => u.Hand.Team.Equals(team));
+
+        return deployments.Sum(d => d.Rank);
+    }
 }
