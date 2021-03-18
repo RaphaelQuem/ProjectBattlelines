5cc7dd5b97c460cdc6213f57fb1e3946dbd04e03
Raphael <Raphzmaia@gmail.com>
2021-03-18 09:32:21 -0300

    Smal changes in battleline

--- Assets/Manager/BattlelineManager.cs
+++ Assets/Manager/BattlelineManager.cs
@@ -8,7 +8,7 @@ public class BattlelineManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.battleline = StaticResources.CurrentBattleline;
     }
 
     // Update is called once per frame
